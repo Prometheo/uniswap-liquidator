4d826ddb3c088b99ecabde7ba4fba31bd04de04a
prometheus <olaoyejnr@gmail.com>
2021-07-15 10:34:00 +0100

    liquidator contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,19 +1,20 @@
 const BonusPepeToken = artifacts.require("BonusPepeToken.sol");
 const LiquidityMigrator = artifacts.require("LiquidityMigrator.sol");
-
+const UniPairAddress = artifacts.require("IUniswapV2Pair.sol")
 
 module.exports = async function (deployer) {
-    await deployer.deploy(Migrations);
-    const bonusPepeToken = await BonusPepeToken.Deployed();
+    await deployer.deploy(BonusPepeToken);
+    const bonusPepeToken = await BonusPepeToken.deployed();
 
-    const routerAddress = '';
-    const pairAddress = '';
-    const routerForkAddress = '';
-    const pairForkAddress = '';
+    const routerAddress = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+    const pairAddress = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
+    const routerForkAddress = '0x204b3e86a41451E0bf4A526Eaaa81F9dB4e6f976';
+    const pairForkAddress = '0xAd7E36dE9a9db9F474Ce9e18D47c45312Ed2580e';
 
     await deployer.deploy(
         LiquidityMigrator,
         routerAddress,
+        pairAddress,
         routerForkAddress,
         pairForkAddress,
         bonusPepeToken.address
